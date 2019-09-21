8b6cb9893d2d541e41d01c3a371bf5fef6a0e729
jlb1999 <jlbroughton88@gmail.com>
2019-09-21 07:20:27 -0400

    added path package

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express();
+var path = require("path");
 
 // MUST UPDATE BUILD FOLDER WHENEVER I PUSH TO HEROKU
 // run this in the client folder....npm run build
