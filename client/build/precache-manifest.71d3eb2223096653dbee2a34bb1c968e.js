42d5d2395265f1163ac8679d42145751f0e6b4d5
jlb1999 <jlbroughton88@gmail.com>
2019-09-21 07:29:28 -0400

    removed OTHER .gitignore build

---
+++ client/build/precache-manifest.71d3eb2223096653dbee2a34bb1c968e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111888c29a26cabddae353a7c3c96d1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcbaa819e01523b88e96",
+    "url": "/static/css/main.bb165cf2.chunk.css"
+  },
+  {
+    "revision": "6b16be4c7f0468627445",
+    "url": "/static/js/2.fc10d4a1.chunk.js"
+  },
+  {
+    "revision": "fcbaa819e01523b88e96",
+    "url": "/static/js/main.3c5468bb.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  }
+]);
