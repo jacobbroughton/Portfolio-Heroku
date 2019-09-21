cc4fe097979201b0fa455a0283e894499ea2feee
jlb1999 <jlbroughton88@gmail.com>
2019-09-21 07:17:57 -0400

    revised build with an app.use for the client side

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const app = express();
 
+// MUST UPDATE BUILD FOLDER WHENEVER I PUSH TO HEROKU
+// run this in the client folder....npm run build
+app.use(express.static(path.join(__dirname, "client/build")))
+
 app.get("/", (req, res) => {
     res.send("Portfolio server")
 })
+
+
 
 app.listen(process.env.PORT || 5004, () => {
     console.log("Server is listening on port 5004")
