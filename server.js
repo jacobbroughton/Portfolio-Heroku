471c663780e01f64f244fa8b9be9d04b57527119
jlb1999 <jlbroughton88@gmail.com>
2019-09-26 08:37:02 -0400

    reverted back

--- server.js
+++ server.js
@@ -10,20 +10,13 @@ app.use(express.static(path.join(__dirname, "client/build")))
 //     res.send("Portfolio server")
 // })
 
-// app.get('/*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'client/public/index.html'), function(err) {
-//       if (err) {
-//         res.status(500).send(err)
-//       }
-//     })
-//   })
-
-app.get('/*', (req, res) => {
-  let url = path.join(__dirname, '../client/build', 'index.html');
-  if (!url.startsWith('/jlbroughton/'))
-    url = url.substring(1);
-  res.sendFile(url);
-});
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/public/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 
 
 app.listen(process.env.PORT || 5004, () => {
