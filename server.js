770295e39d89bb55973ea0ed995501740a5e9288
jlb1999 <jlbroughton88@gmail.com>
2019-10-08 10:55:45 -0400

    new path format in serve

--- server.js
+++ server.js
@@ -4,19 +4,31 @@ var path = require("path");
 
 // MUST UPDATE BUILD FOLDER WHENEVER I PUSH TO HEROKU
 // run this in the client folder....npm run build
-app.use(express.static(path.join(__dirname, "client/build")))
+// app.use(express.static(path.join(__dirname, "client/build")))
 
 // app.get("/", (req, res) => {
 //     res.send("Portfolio server")
 // })
 
-app.get('/contact', function(req, res) {
-  res.send("this is the contact route from the server")
-  })
+// app.get('/contact', function(req, res) {
+//   res.send("this is the contact route from the server")
+//   })
 
-app.get("*", () => {
-  res.sendFile(path.join(__dirname, "/client/public/index.html"))
-})
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
+  })
+} else {
+  app.use(express.static(path.join(__dirname, "/client/public")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"))
+  })
+}
+
+// app.get("*", () => {
+//   res.sendFile(path.join(__dirname, "./client/public/index.html"))
+// })
 
 app.listen(process.env.PORT || 5004, () => {
     console.log("Server is listening on port 5004")
